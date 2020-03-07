8795b0b396eddd1bde35e7523777b433e1e59ee1
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-03-07 10:32:37 -0500

    added conditional logic to search queries

--- API/script.js
+++ API/script.js
@@ -1,45 +1,57 @@
 function showSearch() {
   let city = document.getElementById("city").value
+  let keyword = document.getElementById("keyword").value
 
   EVDB.API.call(
     "/events/search",
     {
       app_key: "x8RFCpSQ55HDvQCp",
       location: city,
-      // keywords: "books",
+      keywords: keyword,
       date: "This Week",
     },
     function(oData) {
       console.log(oData)
-      //   console.log(oData.events.event[i].url)
-      //   let eventPicture = "https:" + oData.events.event[i].image.medium.url
-      //   let cityLocation = oData.events.event[i].city_name
-      //   let eventTitle = oData.events.event[i].title
-      {
-        /* <img src="${eventPicture}">  */
-      }
-      //   let eventUrl = oData.events.event[i].url
-
-      //   let startTime = oData.events.event[i].start_time
-      //   let momentStartTime = moment(startTime)
-      //   let formattedDate = momentStartTime.format("MMMM Do YYYY")
 
       for (let i = 0; i < oData.events.event.length; i++) {
-        let counter = 0
-        console.log("here ya go" + oData.events.event[i])
-
-        counter = counter + i + 1
+        console.log(oData.events.event[i])
 
         let eventUrl = oData.events.event[i].url
         let startTime = oData.events.event[i].start_time
         let momentStartTime = moment(startTime)
-        let formattedDate = momentStartTime.format("MMMM Do YYYY")
+        let formattedDate = momentStartTime.format("MMMM Do, YYYY. h:mm a.")
         let cityLocation = oData.events.event[i].city_name
         let eventTitle = oData.events.event[i].title
-        let eventDescription = oData.events.event[i].description
+        let imgTemplate = ""
+
+        if (
+          oData.events.event[i].image &&
+          oData.events.event[i].image.medium.url
+        ) {
+          let image = oData.events.event[i].image.medium.url
+          let eventPicture = ""
+          if (image.includes("http")) {
+            eventPicture = image
+          } else {
+            eventPicture = "https:" + image
+          }
+
+          imgTemplate = `<p><img src="${eventPicture}"></p>`
+        }
+
+        let description = oData.events.event[i].description
+        let eventDescription = ""
+
+        if (description !== null) {
+          eventDescription = description
+        } else {
+          eventDescription = "No Description Available"
+        }
 
         $("#results").append(
           `
+
+             ${imgTemplate}            
              <p><b>City:</b> ${cityLocation}</p>
              <p><b>Event Title:</b> ${eventTitle}<p>
              <p><b>Event Description: </b>${eventDescription}</p>
