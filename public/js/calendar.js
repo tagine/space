b8901c7f3b9f86952b75b72ec599932d6558b602
tagine <rana@ranafayez.com>
2020-03-12 19:42:44 -0400

    calendar append experiment in calenda.js

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -10,6 +10,27 @@ $(document).ready(function () {
       // orderFriend.order(data);
       console.log(data);
       // logic within here to display the calendar?
+      data.forEach(calendarObject => {
+        //run a conditional to check what key you're on create the new div element then append it to card info in line 15 and 16
+        let cardContent =  $("#cardinfo");
+        let listingDiv = cardContent.append("<div></div>");
+        listingDiv.addClass("card");
+        Object.entries(calendarObject).forEach(([key,value]) => {
+          if(key==="title"){
+            console.log(value);
+            let titleDiv = listingDiv.append("<div></div>");
+            let title = titleDiv.append("<h3></h3>").text(value);
+          }
+          // if(key==="date"){
+          //   console.log(value);
+          //   let dateDiv = dateDiv.append("<div></div>");
+          //   let date = dateDiv.append("<p></p>").text(value);
+          // }
+          // let imageDiv = listingDiv.append("<div></div>");
+          // let image = imageDiv.append("<img></img>").attr("src", src1);
+          // console.log(value);
+        })
+        });
     });
   }
 
