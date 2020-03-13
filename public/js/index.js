c8419f8397f526f365928c88a90de1b7c89209dc
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-03-13 17:37:16 -0400

    Fixed up code so app now works

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,37 @@
-$(document).ready(function () {
-  // $(function () {
-  $(document).on("click", ".add", function (event) {
-    event.preventDefault();
-    let targetId = event.target.id;
-    EVDB.API.call(
-      "/events/get",
-      {
-        app_key: "x8RFCpSQ55HDvQCp",
-        id: targetId
-      },
-      function (oData) {
-        console.log(oData);
-        const newEvent = {
-          title: oData.title,
-          date: oData.start_time,
-          description: oData.description,
-          time: oData.start_time,
-          location: oData.city,
-          link: oData.url,
-          imageLink: oData.images.image.thumb.url
-        }
-        postEvent(newEvent);
-      })
-  });
+// const postEvent = event => {
+//   // post route will need to be changed for heroku deployment??
+//   $.post("/api/events", event, () => {
+//     console.log("successfully posted")
+//   })
+// }
 
-  const postEvent = (event) => {
-    // post route will need to be changed for heroku deployment??
-    $.post("/api/events", event, () => {
-      console.log("successfully posted")
-      // window redirect?
-      // window.location.href = "/calendar";
-    });
-  }
+// function getEvent(event) {
+//   event.preventDefault()
+//   let targetId = event.target.id
+//   EVDB.API.call(
+//     "/events/get",
+//     {
+//       app_key: "x8RFCpSQ55HDvQCp",
+//       id: targetId,
+//     },
+//     function(oData) {
+//       console.log(oData)
+//       const newEvent = {
+//         title: oData.title,
+//         date: oData.start_time,
+//         description: oData.description,
+//         time: oData.start_time,
+//         location: oData.city,
+//         link: oData.url,
+//         imageLink: oData.images.image.thumb.url,
+//       }
+//       postEvent(newEvent)
+//     }
+//   )
+// }
 
-
-});
+// $(document).ready(function() {
+//   // $(function () {
+//   $(document).on("click", ".add",
+//   })
+// })
